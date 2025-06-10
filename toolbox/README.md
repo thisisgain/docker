178df43515c95f75fada6ee85d8e1ef249a69b26
Alberto Contreras <a.contreras@catchdigital.com>
2025-06-10 15:53:06 +0100

    Bump final versions of the images.

--- toolbox/README.md
+++ toolbox/README.md
@@ -9,14 +9,21 @@ This image is based on php 8.3-fpm
 Set of tools to run for building several kind of projects.
 
 Tools in this image:
-* aws cli: 2.22.7
-* aws cdk: 2.171.1
-* aws amplify: 12.13.1
-* eb: 3.21.0
-* php: 8.3.14-fpm
-* node: v20.18.1
-* npm: 10.8.2
-* composer: 2.8.3
+* aws cli: 2.27.32
+* aws cdk: 2.1018.0
+* aws amplify: 13.0.1
+* eb: 3.24.1
+* php: 8.3.22-fpm
+* nvm: 0.39.7
+  * node: v20.18.1
+  * npm: 10.8.2
+* composer: 2.8.9
 * python3: 3.11.2
-* ansible: 2.18.0
-* acli: 2.35.1
+* ansible: 2.18.6
+* acli: 2.42.0
+
+## NVM
+
+Node is set up via nvm. The version above is the default version. If a different version is required, you can set an environment variable "NODE_VERSION" with the specific node version required for run of the container.
+
+Example: `docker run --rm -ti -e NODE_VERSION=16 thisisgain/toolbox:1.6.0 node --version`
--- toolbox/docker-entrypoint.sh
+++ toolbox/docker-entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Check if NODE_VERSION environment variable is set and different from default
-if [ -n "$NODE_VERSION" ] && [ "$NODE_VERSION" != "$DEFAULT_NODE_VERSION" ]; then
-  echo "Installing Node.js $NODE_VERSION..."
-  export NVM_DIR=/usr/local/.nvm
-  # Source the nvm.sh script to make nvm command available
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+# Check if NODE_VERSION environment variable is set
+if [ -n "$NODE_VERSION" ]; then
+  # Get current version without the "v" prefix
+  CURRENT_VERSION=$(node -v 2>/dev/null | sed "s/^v//")
   
-  # Now we can use nvm
-  nvm install $NODE_VERSION && nvm alias default $NODE_VERSION && nvm use default
-  
-  # Update symlinks
-  ln -sf $NVM_DIR/versions/node/v$NODE_VERSION/bin/node /usr/local/bin/node
-  ln -sf $NVM_DIR/versions/node/v$NODE_VERSION/bin/npm /usr/local/bin/npm
-  ln -sf $NVM_DIR/versions/node/v$NODE_VERSION/bin/npx /usr/local/bin/npx
+  # Check if versions are different (accounting for version format differences)
+  if [[ "$CURRENT_VERSION" != "$NODE_VERSION"* ]]; then
+    echo "Switching from Node.js $CURRENT_VERSION to $NODE_VERSION"
+    export NVM_DIR=/usr/local/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+    
+    # Install and use the specified version
+    nvm install "$NODE_VERSION" >/dev/null 2>&1
+    nvm use "$NODE_VERSION" >/dev/null 2>&1
+    nvm alias default "$NODE_VERSION" >/dev/null 2>&1
+    
+    # Update symlinks
+    ln -sf $NVM_DIR/versions/node/v$NODE_VERSION/bin/node /usr/local/bin/node
+    ln -sf $NVM_DIR/versions/node/v$NODE_VERSION/bin/npm /usr/local/bin/npm
+    ln -sf $NVM_DIR/versions/node/v$NODE_VERSION/bin/npx /usr/local/bin/npx
+  fi
 fi
 
 # Execute the command passed to the container
